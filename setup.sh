5bccb5c0dd521196a983e9170f74d3d197c5edaa
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-04-24 12:56:12 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,7 @@ if uname | grep -q "^MINGW"; then
   cmd=dbeaverc
 fi
 cmd -nosplash -application org.eclipse.equinox.p2.director -repository http://vrapper.sourceforge.net/update-site/stable -installIU net.sourceforge.vrapper.core
+
+cmd -nosplash -application org.eclipse.equinox.p2.director -repository https://matf.github.io/relativenumberruler/updatesite -installIU com.github.matf.relativenumberruler
+ 
 curl https://raw.githubusercontent.com/chubbyhippo/.vrapperrc/refs/heads/main/.vrapperrc -o ~/.vrapperrc
