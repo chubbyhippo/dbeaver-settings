99fa09f89df4568555f2094bcfbd73e494f7e8ec
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-04-24 12:41:10 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ if uname | grep -q "^MINGW"; then
   cmd=dbeaverc
 fi
 cmd -nosplash -application org.eclipse.equinox.p2.director -repository http://vrapper.sourceforge.net/update-site/stable -installIU net.sourceforge.vrapper.core
+curl https://raw.githubusercontent.com/chubbyhippo/.vrapperrc/refs/heads/main/.vrapperrc -o ~/.vrapperrc
