f1fc046866d0f5d6eaf2a9c8d0545cab27b49817
chubbyhippo <58356115+chubbyhippo@users.noreply.github.com>
2026-04-25 08:50:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 dbeaver -nosplash -application org.eclipse.equinox.p2.director -repository https://vrapper.sourceforge.net/update-site/unstable -installIU net.sourceforge.vrapper.feature.group
+dbeaver -nosplash -application org.eclipse.equinox.p2.director -repository https://vrapper.sourceforge.net/update-site/unstable -installIU net.sourceforge.vrapper.plugin.argtextobj.feature.group
 
 dbeaver -nosplash -application org.eclipse.equinox.p2.director -repository https://matf.github.io/relativenumberruler/updatesite -installIU com.github.matf.relativenumberruler
  
